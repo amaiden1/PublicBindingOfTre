beb9956209dd8b42cc5e90be850eeb2b497d59f5
Nolan <nolan.i822@gmail.com>
2018-03-13 21:07:12 -0400

    Adding new stuff

--- src/bindingofisaac/ImageHelper.java
+++ src/bindingofisaac/ImageHelper.java
@@ -1,14 +1,14 @@
 package bindingofisaac;
 
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 import javax.swing.text.Element;
-import javax.swing.text.html.ImageView;
 
 public class ImageHelper {
 
 	public static ImageView imageFromSource(String filename) {
-		return new ImageView((Element) new Image(Image.class.getResource(filename).toExternalForm()));
+		return new ImageView(new Image(Image.class.getResource(filename).toExternalForm()));
 	}
 
 }
---
+++ src/bindingofisaac/Room.java
@@ -0,0 +1,17 @@
+package bindingofisaac;
+
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+
+public class Room {
+
+	private double currPlayerX, currPlayerY;
+	private final int ROOM_WIDTH = 1040; // 13 * 80
+	private final int ROOM_HEIGHT = 560; // 7 * 80
+	private final ImageView BACKGROUND = ImageHelper.imageFromSource("room.png");
+
+
+
+
+
+}
