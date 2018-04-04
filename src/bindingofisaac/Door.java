2a323aea495c68f1cee82135028596a77a91f9bb
Nolan <nolan.i822@gmail.com>
2018-04-04 13:34:13 -0400

    A few tweaks

--- src/bindingofisaac/Door.java
+++ src/bindingofisaac/Door.java
@@ -17,30 +17,32 @@ public class Door {
 		doorY,
 		hitboxX,
 		hitboxY;
-	private Room destination;
+	//private Room destination;
 	private boolean isOpen;
 	private ImageView img;  // this image is first provided UP
 	private Rectangle hitbox;
 
-	public Door(int dir, Room dest, ImageView image) {
+	public Door(int dir) {
+
 		this.direction = dir;
-		this.destination = dest;
-		this.img = image;
 		img.setFitHeight(ROOM_INNER_OFFSET);
+
 		if (dir == UP) {
 			doorX = ROOM_WIDTH / 2;
 			doorY = ROOM_INNER_OFFSET / 2;
 			hitbox = new Rectangle(img.getFitWidth(),15);
 			hitboxX = ROOM_WIDTH / 2;
 			hitboxY = (int)(ROOM_INNER_OFFSET + (hitbox.getHeight() / 2));
+			img = DOOR_UP;
 
-		} else if (dir == Constants.RIGHT) {
+		} else if (dir == RIGHT) {
 			img.setRotate(90);  // rotate to face RIGHT
 			doorX = ROOM_WIDTH - (ROOM_INNER_OFFSET / 2);
 			doorY = Constants.ROOM_HEIGHT / 2;
 			hitbox = new Rectangle(15, img.getFitHeight());
 			hitboxX = (int)(ROOM_WIDTH - ROOM_INNER_OFFSET - (hitbox.getWidth() / 2));
 			hitboxY = ROOM_HEIGHT / 2;
+			img = DOOR_RIGHT;
 
 		} else if (dir == DOWN) {
 			img.setRotate(180);  // rotate to face DOWN
@@ -49,6 +51,7 @@ public class Door {
 			hitbox = new Rectangle(15, img.getFitHeight());
 			hitboxX = ROOM_WIDTH / 2;
 			hitboxY = (int)(ROOM_HEIGHT - ROOM_INNER_OFFSET - (hitbox.getHeight() / 2));
+			img = DOOR_DOWN;
 
 		} else if (dir == LEFT) {
 			img.setRotate(270);  // rotate to face LEFT
@@ -56,6 +59,7 @@ public class Door {
 			doorY = ROOM_HEIGHT / 2;
 			hitboxX = (int)(ROOM_INNER_OFFSET + (hitbox.getWidth() / 2));
 			hitboxY = ROOM_HEIGHT / 2;
+			img = DOOR_LEFT;
 
 		} else {
 			// Direction isn't 0, 1, 2, 3
@@ -70,10 +74,15 @@ public class Door {
 	public boolean isOpen() {
 		return isOpen;
 	}
-
+	/*
 	public Room getDestination() {
 		return destination;
 	}
+
+	public void setDestination(Room dest) {
+		destination = dest;
+	}
+	*/
 
 	public ImageView getImageView() {
 		return img;
