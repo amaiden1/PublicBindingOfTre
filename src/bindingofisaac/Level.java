8fe821411746e0a6e9ddea8a65ddaff8845f2a18
Nolan <nolan.i822@gmail.com>
2018-04-04 13:34:37 -0400

    Currently still a stub

--- src/bindingofisaac/Level.java
+++ src/bindingofisaac/Level.java
@@ -1,7 +1,13 @@
 package bindingofisaac;
 
+import java.util.ArrayList;
+
 public class Level {
 
-	
+	private ArrayList<Room> rooms;
+	private Room currentRoom;
+	private RoomGenerator roomGen;
+	private LayoutGenerator layoutGen;
+
 
 }
