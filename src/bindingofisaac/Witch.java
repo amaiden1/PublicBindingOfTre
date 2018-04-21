cad37ae022b38ba44701c40371a4465f595eb9c0
Nolan <nolan.i822@gmail.com>
2018-04-21 01:47:32 -0400

    added shoot stuff

--- src/bindingofisaac/Witch.java
+++ src/bindingofisaac/Witch.java
@@ -9,6 +9,9 @@ import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.image.ImageView;
+
+import java.sql.Time;
+
 import static bindingofisaac.Constants.*;
 
 /**
@@ -18,6 +21,8 @@ import static bindingofisaac.Constants.*;
 public class Witch extends Enemy{
 	
 	private int deltaY;
+	private String bulletImg;
+	private int bulletTimerIndex;
 	
 	public Witch(int floorLevel, Room givenRoom){
 		deltaY = 1;
@@ -28,25 +33,41 @@ public class Witch extends Enemy{
 		health = 10 + (10 * floorLevel);
 		damage = 5 + (2 * (floorLevel - 1));
 		speed = 1;
+		bulletImg = "/img/lightning_bolt.png";
 		
 		System.out.println("witch location: " + x + " , " + y);
 		System.out.println("sprite location: " + sprite.getX() + " , " + sprite.getY());
 		
-		timerIndex = Main.player.getGame().getController().getTimer().addTick(5 , Timeline.INDEFINITE, new EventHandler<ActionEvent>(){
+		timerIndex = Main.player.getGame().getController().getTimer().addTick(10 , Timeline.INDEFINITE, new EventHandler<ActionEvent>(){
             public void handle(ActionEvent ae){
                 updatePos();
 				checkCollision();
             }
         });
+		bulletTimerIndex = Main.player.getGame().getController().getTimer().addTick(1000, Timeline.INDEFINITE, new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				shoot();
+			} //currently this teleports image of bullet to same spot on map. The pythagorean method does not work properly
+		});
 		
 	}
 	
 	@Override
 	public void updatePos(){
-		deltaY += (y <= 100)? 1 : (deltaY = (y >= 600)? -1 : deltaY);
+		System.out.println("X: " + x + ", Y: + " + y);
+		deltaY = (y <= 100)? 1 : (deltaY = (y >= 600)? -1 : deltaY);
 		y += deltaY;
 		sprite.relocate(x, y);
 	}
 	
-	
+	@Override
+	public void shoot() {
+		System.out.println("shooting!!!!");
+		Bullet bullet = new Bullet(x, y, Main.player.getX(), Main.player.getY(), bulletImg);
+	}
+
+	public int getBulletTimerIndex() {
+		return bulletTimerIndex;
+	}
 }
