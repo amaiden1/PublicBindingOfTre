7592ab62e2499086c9840be3134b5e943889bf39
Nolan <nolan.i822@gmail.com>
2018-04-20 21:10:27 -0400

    Updated class with useful things

--- src/bindingofisaac/Item.java
+++ src/bindingofisaac/Item.java
@@ -1,10 +1,43 @@
 package bindingofisaac;
 
+import javafx.scene.image.ImageView;
+
 public class Item {
 
-	/*
-	 * Stub class.
-	 * To be implemented later.
-	 */
+	private int healthDelta;
+	private int speedDelta;
+	private int damageDelta;
+	private int attackSpeedDelta;
+	private String itemName;
+	private ImageView itemImg;
 
+	public Item(ImageView img, int health, int speed, int damage, int attackSpeed, String name) {
+
+		itemName = name;
+		healthDelta = health;
+		speedDelta = speed;
+		damageDelta = damage;
+		attackSpeedDelta = attackSpeed;
+		itemImg = img;
+	}
+
+	public int getHealthDelta() {
+		return healthDelta;
+	}
+
+	public int getSpeedDelta() {
+		return speedDelta;
+	}
+
+	public int getDamageDelta() {
+		return damageDelta;
+	}
+
+	public int getAttackSpeedDelta() {
+		return attackSpeedDelta;
+	}
+
+	public ImageView getImageView() {
+		return itemImg;
+	}
 }
