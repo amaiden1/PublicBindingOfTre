7f1249ebba49f4ce0e27b91641da07e86bd18c7e
Nolan <nolan.i822@gmail.com>
2018-04-22 17:21:50 -0400

    made shooting work

--- src/bindingofisaac/Bullet.java
+++ src/bindingofisaac/Bullet.java
@@ -5,15 +5,10 @@
  */
 package bindingofisaac;
 
+import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
-import javafx.application.Platform;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Pane;
-
-import java.security.interfaces.DSAPrivateKey;
-import java.util.ArrayList;
+import javafx.util.Duration;
 
 /**
  *
@@ -28,22 +23,26 @@ public class Bullet {
 	private double startY;
 	private double endX;
 	private double endY;
-	ArrayList<Double> xStepList;
-	ArrayList<Double> yStepList;
-	double distEnemyToCorner;
-	double distCornerToPlayer;
-	int triangleNum;
-	double baseFactor;
-	double heightFactor;
-	int ticks;
-	int currentPosIndex;
+	private double x;
+	private double y;
 
-	public Bullet(double startX, double startY, double destinationX, double destinationY, String imageSrc){
-		bulletSprite = new ImageView(imageSrc);
-		bulletSprite.setFitHeight(30);
-		bulletSprite.setFitWidth(30);
+	private int tickLength;
+	private int travelTime;
+	private int steps;
+	private double xDist;
+	private double yDist;
+	private double xStep;
+	private double yStep;
+	private double cornerX;
+	private double cornerY;
+	private int totalProgress;
+
+	public Bullet(double startX, double startY, double destinationX, double destinationY, int travelTime, int tickLength, String imageSrc){
+
 		try{
 			bulletSprite = new ImageView(imageSrc);
+			bulletSprite.setPreserveRatio(true);
+			bulletSprite.setFitHeight(10);
 		}
 		catch(Exception e){
 			System.out.println("bullet image not found... using default");
@@ -51,67 +50,70 @@ public class Bullet {
 		}
 		this.startX = startX;
 		this.startY = startY;
-		this.endX = Main.player.getX();
-		this.endY = Main.player.getY();
+		this.endX = destinationX;
+		this.endY = destinationY;
+		this.x = startX;
+		this.y = startY;
 
 		Main.player.getCurrentRoom().getRoomPane().getChildren().add(bulletSprite);
 		bulletSprite.relocate(startX,startY);
-		bulletSprite.setRotate(90);
-
-		ticks = 50;
-
-		timerIndex = Main.player.getGame().getController().getTimer().addTickAndPlay(5, ticks, new EventHandler<ActionEvent>(){
-            public void handle(ActionEvent ae){
-                updateBullet();
-            }
-        });
 
 		// shooting stuff
-		xStepList = new ArrayList<>();
-		yStepList = new ArrayList<>();
-		distEnemyToCorner = getDistance(startX, startY, startX, endY);
-		distCornerToPlayer = getDistance(startX, endY, endX, endY);
-		baseFactor = distEnemyToCorner / ticks;
-		heightFactor = distCornerToPlayer / ticks;
-		for (int b = 0 ; b < distEnemyToCorner ; b++) {
-			xStepList.add(baseFactor * b);
-			System.out.println("x step loop ran!");
-		}
-		for (int h = 0 ; h < distCornerToPlayer ; h++) {
-			yStepList.add(heightFactor * h);
-			System.out.println("y step loop ran!");
-		}
-		System.out.println("dist enemy to corner: " + distEnemyToCorner);
-		System.out.println("dist corner to player: " + distCornerToPlayer);
-		System.out.println("x length: " + xStepList.size() + " y length: " + yStepList.size());
-		currentPosIndex = 0;
 
-		
+		cornerX = endX;
+		cornerY = startY;
+		this.tickLength = tickLength;
+		this.travelTime = travelTime;
+		steps = travelTime / tickLength;
+
+		xDist = cornerX - startX;
+		yDist = endY - cornerY;
+		xStep = xDist / steps;
+		yStep = yDist / steps;
+
+		System.out.println("======== INIT VALUES ========");
+		System.out.println("startX: " + startX);
+		System.out.println("startY: " + startY);
+		System.out.println("endX: " + endX);
+		System.out.println("endY: " + endY);
+		System.out.println("cornerX: " + cornerX);
+		System.out.println("cornerY: " + cornerY);
+		System.out.println("tickLength: " + tickLength);
+		System.out.println("travelTime: " + travelTime);
+		System.out.println("steps: " + steps);
+		System.out.println("xDist: " + xDist);
+		System.out.println("yDist: " + yDist);
+		System.out.println("xStep: " + xStep);
+		System.out.println("yStep: " + yStep);
+
+		totalProgress = 0;
+		timerIndex = Main.player.getGame().getController().getTimer().addTickAndPlay(tickLength, steps, event -> {
+			totalProgress += tickLength;
+			updateBullet();
+		});
+
 	}
 
-	/*
-	SOLUTION IN PROGRESS I AM WORKING ON!
-	It aims to divide the area up into a bunch of triangles.
-	Please see the picture I sent you in text.
-	See if you might be able to get this to work.
-	 */
+	private void updateBullet(){
 
-	private double getDistance(double x1, double y1, double x2, double y2) {
-		double xDist = Math.pow((x2 - x1), 2);
-		double yDist = Math.pow((y2 - y1), 2);
-		double sqrt = Math.sqrt(xDist - yDist);
-		return sqrt;
-	}
-	
-	public void updateBullet(){
-
-		bulletSprite.relocate(xStepList.get(currentPosIndex), yStepList.get(currentPosIndex));
-		if (xStepList.get(currentPosIndex + 1) != null && yStepList.get(currentPosIndex + 1) != null) {
-			// there is another position
-			currentPosIndex++;
-		} else {
-			// not another position
-			// remove the bullet, but for now do nothing
+		x += xStep;
+		y += yStep;
+		bulletSprite.relocate(x, y);
+		checkCollision();
+		if (totalProgress >= travelTime) {
+			despawn();
 		}
+
+	}
+
+	private void checkCollision(){
+		if((Main.player.getImageView().getBoundsInParent().intersects(bulletSprite.getBoundsInParent())) && Main.player != null){
+			despawn();
+		}
+	}
+
+	private void despawn() {
+		Main.player.getGame().getController().getTimer().removeNull(timerIndex);
+		Main.player.getCurrentRoom().getRoomPane().getChildren().remove(bulletSprite);
 	}
 }
