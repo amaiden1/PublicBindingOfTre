8d6d6ef14336190de5694c8be96d45f1a44681af
Nolan <nolan.i822@gmail.com>
2018-04-24 04:38:35 -0400

    fixed NPE for double despawning

--- src/bindingofisaac/PlayerBullet.java
+++ src/bindingofisaac/PlayerBullet.java
@@ -6,7 +6,8 @@
 package bindingofisaac;
 
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+
+import java.util.ArrayList;
 
 /**
  *
@@ -14,20 +15,29 @@ import javafx.scene.image.ImageView;
  */
 public class PlayerBullet extends Bullet {
 
+	private boolean alreadyDespawned;
+
     public PlayerBullet(double startX, double startY, double destinationX, double destinationY, int travelTime, int tickLength, Image imageSrc){
             super(startX, startY, destinationX, destinationY, travelTime, tickLength, imageSrc);
+            this.alreadyDespawned = false;
     }
 
     @Override
     protected void checkCollision(){
         if (Main.player.getCurrentRoom().getEnemies() != null && Main.player.getCurrentRoom().getEnemies().size() > 0) {
-            for (Enemy enemy : Main.player.getCurrentRoom().getEnemies()) {
-                if (super.getBulletSprite().getBoundsInParent().intersects(enemy.getSprite().getBoundsInParent())) {
+	        ArrayList<Enemy> enemiesToCheck = Main.player.getCurrentRoom().getEnemies();
+	        // Must be a regular for loop to avoid ConcurrentModificationExceptions
+            for (int i = 0 ; i < enemiesToCheck.size() ; i++) {
+                if (super.getBulletSprite().getBoundsInParent().intersects(enemiesToCheck.get(i).getSprite().getBoundsInParent())) {
                     System.out.println("intersects");
-                    enemy.takeDamage(Main.player.getDamage());
-                    despawn();
-                    if (enemy.isDead()) {
-                        Main.player.getCurrentRoom().getEnemies().remove(enemy);
+	                enemiesToCheck.get(i).takeDamage(Main.player.getDamage());
+                    System.out.println(this +" despawn called to collision");
+                    if (!this.alreadyDespawned) {
+                    	despawn();
+                    	this.alreadyDespawned = true;
+                    }
+                    if (enemiesToCheck.get(i).isDead()) {
+                        Main.player.getCurrentRoom().getEnemies().remove(enemiesToCheck.get(i));
                         if(Main.player.getCurrentRoom().getEnemies().size() == 0){ // != 0
                             Main.player.getCurrentRoom().setCleared(true);
                         }
